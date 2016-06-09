ca47df505cfcf4843d977b57479b66320e23d72b
Gonzalo <gonzaponte@gmail.com>
2016-06-09 13:56:46 +0200

    test improved

--- GLIB/main.cxx
+++ GLIB/main.cxx
@@ -2,23 +2,35 @@
 
 int main(void)
 {
-  //std::cout << "HOLA" << std::endl;
-  int s = 10;
+  std::cout << "HOLA" << std::endl;
+  int s   = 10;
   int x[] = {0,1,2,3,4,5,6,7,8,9};
-  glib::Vector<int> a( x, s );
-  //glib::Vector<int> a( x, x + s );
-  glib::Vector<int> b = a;// + 5;
-  glib::Vector<int> c;
+  glib::Vector<int> a( s, x );
+  glib::Vector<int> b( s, 2 );
+  glib::Vector<int> c = a;// + 5;
+  glib::Vector<int> d;
 
-  a = a / 1;
-  b *= 2;
-  b = b.Slice(0,s);
-  a += 1;
-  c = a.Slice(0,s,2);
-  a.Swap(0,-1);
-  for ( int i=0; i<a.size(); i++ ) std::cout << i << " " << a[i] << " " << b[i] << std::endl;
-  for ( int i=1; i<=a.size(); i++ ) std::cout << i << " " << a[-i]<< std::endl;
-  std::cout << b << std::endl;
-  std::cout << c << std::endl;
+  std::cout << "a:                  " << a                  << std::endl;
+  std::cout << "b:                  " << b                  << std::endl;
+  std::cout << "c:                  " << c                  << std::endl;
+  std::cout << "a+2:                " << a+2                << std::endl;
+  std::cout << "b-2:                " << b-2                << std::endl;
+  std::cout << "c*2:                " << c*2                << std::endl;
+  std::cout << "a/2:                " << a/2                << std::endl;
+  std::cout << "b+c:                " << b+c                << std::endl;
+  std::cout << "b-c:                " << b-c                << std::endl;
+  std::cout << "b*c:                " << b*c                << std::endl;
+  std::cout << "c/b:                " << c/b                << std::endl;
+  std::cout << "a.Slice(0,4):       " << a.Slice(0,4)       << std::endl;
+  std::cout << "a.Slice(-11,9,-1):  " << a.Slice(-11,9,-1)  << std::endl; a.Swap(0,-1);
+  std::cout << "a.Swap(0,-1):       " << a                  << std::endl;
+  std::cout << "a.Reverse:          " << a.Reverse()        << std::endl; a[0] = 110;
+  std::cout << "a[0] = 110:         " << a                  << std::endl;
+  std::cout << "b.Concatenate(c):   " << b.Concatenate(c)   << std::endl;
+  std::cout << "b.Sum():            " << b.Sum()            << std::endl;
+  std::cout << "b.Mod():            " << b.Mod()            << std::endl;
+  std::cout << "b.Mod2():           " << b.Mod2()           << std::endl;
+  std::cout << "b.Dot(b):           " << b.Dot(b)           << std::endl;
+  std::cout << "b.Slice(0,1).Pop(): " << b.Slice(0,1).Pop() << std::endl;
   return 0;
 }
